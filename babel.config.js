54a3202ce6b701918cd2c424b5869a6e0e8515e3
Tim Broder <timothy.broder@gmail.com>
2025-12-26 14:26:52 -0500

    fix sim

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['hot-updater/babel-plugin'],
+  plugins: [
+    'hot-updater/babel-plugin',
+    'react-native-reanimated/plugin', // Must be last
+  ],
 };
